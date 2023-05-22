7fb468d0405ac26392f3774e16ca6f85ba3be678
WelberLopes <deunatelhacontato01@gmail.com>
2023-05-22 19:28:08 -0300

    Problema 1: Arte ASCII Simples

---
+++ Arte_ASCII.java
@@ -0,0 +1,14 @@
+
+public class Arte_ASCII {
+    public static void main(String[] args) {
+        System.out.println("  ####  ");
+        System.out.println(" #    #");
+        System.out.println("# #  # #");
+        System.out.println("#      #");
+        System.out.println("# #  # # ");
+        System.out.println("#  ##  #");
+        System.out.println(" #    # ");
+        System.out.println("  ####  ");
+
+    }
+}
