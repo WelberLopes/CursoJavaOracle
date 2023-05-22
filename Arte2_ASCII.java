4c5679b517373b59bb8320327b782b77f4ce6e57
WelberLopes <deunatelhacontato01@gmail.com>
2023-05-22 19:55:51 -0300

    Problema 2: Arte ASCII Original

---
+++ Arte2_ASCII.java
@@ -0,0 +1,12 @@
+public class Arte2_ASCII {
+    public static void main(String[] args) {
+        System.out.println("    /\\          /\\");
+        System.out.println("   /  \\________/  \\  ");
+        System.out.println("  /                 \\ ");
+        System.out.println(" (  /\\          /\\  )");
+        System.out.println(" ====      V       ====");
+        System.out.println(" =====  (__|__)  ======");
+        System.out.println("    (              )  ");
+        System.out.println("      (__________)   ");
+    }
+}
