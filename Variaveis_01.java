784b732415060142a1d46177c2e0466e2f8e5fd9
WelberLopes <deunatelhacontato01@gmail.com>
2023-05-22 22:28:04 -0300

    Atividades Curso Java da Oracle

---
+++ Variaveis_01.java
@@ -0,0 +1,12 @@
+public class Variaveis_01 {
+    public static void main(String[] args) {
+        String x = "puppy \npuppy";
+        System.out.println(x);
+
+        x = "kitty \n kitty";
+        System.out.println(x);
+
+        x = "kitty \nbunny";
+        System.out.println(x);
+    }
+}
